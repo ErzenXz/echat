21f0fcdc392f35461ea5480c909406b45c850e62
ErzenXs <njnana2017@gmail.com>
2022-08-02 18:09:31 +0200

    Small changes and bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# echat
+# eChat
+
+## Features:
+    - Group chat
+    - Create and join rooms
+    - Login or create an account
+    - Bad words filter
+    - Send images, videos and links
+    - Fast and free chat
+
--- js/messeges.js
+++ js/messeges.js
@@ -5,7 +5,7 @@ const room = localStorage.getItem("room"); // Room Code
 const ul = document.querySelector("#ulist");
 const username = localStorage.getItem("username"); // Username
 const email = localStorage.getItem("uName"); // User Email Address
-const query = firebase.database().ref("rooms/" + room + "/chats").limitToLast(50);
+const query = firebase.database().ref("rooms/" + room + "/chats").limitToLast(75);
 const rooms = firebase.database().ref("rooms/");
 const roomName = firebase.database().ref("rooms/" + room);
 const bannedwords = ["ahole", "anus", "ash0le", "ash0les", "asholes", "ass", "Ass Monkey", "Assface", "assh0le", "assh0lez", "asshole", "assholes", "assholz", "asswipe", "azzhole", "bassterds", "bastard", "bastards", "bastardz", "basterds", "basterdz", "Biatch", "bitch", "bitches", "Blow Job", "boffing", "butthole", "buttwipe", "c0ck", "c0cks", "c0k", "Carpet Muncher", "cawk", "cawks", "Clit", "cnts", "cntz", "cock", "cockhead", "cock-head", "cocks", "CockSucker", "cock-sucker", "crap", "cum", "cunt", "cunts", "cuntz", "dick", "dild0", "dild0s", "dildo", "dildos", "dilld0", "dilld0s", "dominatricks", "dominatrics", "dominatrix", "dyke", "enema", "f u c k", "f u c k e r", "fag", "fag1t", "faget", "fagg1t", "faggit", "faggot", "fagg0t", "fagit", "fags", "fagz", "faig", "faigs", "fart", "flipping the bird", "fuck", "fucker", "fuckin", "fucking", "fucks", "Fudge Packer", "fuk", "Fukah", "Fuken", "fuker", "Fukin", "Fukk", "Fukkah", "Fukken", "Fukker", "Fukkin", "g00k", "God-damned", "h00r", "h0ar", "h0re", "hells", "hoar", "hoor", "hoore", "jackoff", "jap", "japs", "jerk-off", "jisim", "jiss", "jizm", "jizz", "knob", "knobs", "knobz", "kunt", "kunts", "kuntz", "Lezzian", "Lipshits", "Lipshitz", "masochist", "masokist", "massterbait", "masstrbait", "masstrbate", "masterbaiter", "masterbate", "masterbates", "Motha Fucker", "Motha Fuker", "Motha Fukkah", "Motha Fukker", "Mother Fucker", "Mother Fukah", "Mother Fuker", "Mother Fukkah", "Mother Fukker", "mother-fucker", "Mutha Fucker", "Mutha Fukah", "Mutha Fuker", "Mutha Fukkah", "Mutha Fukker", "n1gr", "nastt", "nigger;", "nigur;", "niiger;", "niigr;", "orafis", "orgasim;", "orgasm", "orgasum", "oriface", "orifice", "orifiss", "packi", "packie", "packy", "paki", "pakie", "paky", "pecker", "peeenus", "peeenusss", "peenus", "peinus", "pen1s", "penas", "penis", "penis-breath", "penus", "penuus", "Phuc", "Phuck", "Phuk", "Phuker", "Phukker", "polac", "polack", "polak", "Poonani", "pr1c", "pr1ck", "pr1k", "pusse", "pussee", "pussy", "puuke", "puuker", "queer", "queers", "queerz", "qweers", "qweerz", "qweir", "recktum", "rectum", "retard", "sadist", "scank", "schlong", "screwing", "semen", "sex", "sexy", "Sh!t", "sh1t", "sh1ter", "sh1ts", "sh1tter", "sh1tz", "shit", "shits", "shitter", "Shitty", "Shity", "shitz", "Shyt", "Shyte", "Shytty", "Shyty", "skanck", "skank", "skankee", "skankey", "skanks", "Skanky", "slag", "slut", "sluts", "Slutty", "slutz", "son-of-a-bitch", "turd", "va1jina", "vag1na", "vagiina", "vagina", "vaj1na", "vajina", "vullva", "vulva", "w0p", "wh00r", "wh0re", "whore", "xrated", "xxx", "b!+ch", "bitch", "blowjob", "clit", "arschloch", "fuck", "shit", "ass", "asshole", "b!tch", "b17ch", "b1tch", "bastard", "bi+ch", "boiolas", "buceta", "c0ck", "cawk", "chink", "cipa", "clits", "cock", "cum", "cunt", "dildo", "dirsa", "ejakulate", "fatass", "fcuk", "fuk", "fux0r", "hoer", "hore", "jism", "kawk", "l3itch", "l3i+ch", "lesbian", "masturbate", "masterbat*", "masterbat3", "motherfucker", "s.o.b.", "mofo", "nazi", "nigga", "nigger", "nutsack", "phuck", "pimpis", "pusse", "pussy", "scrotum", "sh!t", "shemale", "shi+", "sh!+", "slut", "smut", "teets", "boobs", "b00bs", "teez", "testical", "testicle", "w00se", "jackoff", "wank", "whoar", "whore", "*damn", "*dyke", "*fuck*", "*shit*", "@$$", "amcik", "andskota", "arse*", "assrammer", "ayir", "bi7ch", "bitch*", "bollock*", "breasts", "butt-pirate", "cabron", "cazzo", "chraa", "chuj", "Cock*", "cunt*", "d4mn", "daygo", "dego", "dick*", "dike*", "dupa", "dziwka", "ejackulate", "Ekrem*", "Ekto", "enculer", "faen", "fag*", "fanculo", "fanny", "feces", "feg", "Felcher", "ficken", "fitt*", "Flikker", "foreskin", "Fotze", "Fu(*", "fuk*", "futkretzn", "gook", "guiena", "h0r", "h4x0r", "helvete", "hoer*", "honkey", "Huevon", "hui", "injun", "jizz", "kanker*", "kike", "klootzak", "kraut", "knulle", "kuk", "kuksuger", "Kurac", "kurwa", "kusi*", "kyrpa*", "lesbo", "mamhoon", "masturbat*", "merd*", "mibun", "monkleigh", "mouliewop", "muie", "mulkku", "muschi", "nazis", "nepesaurio", "nigger*", "orospu", "paska*", "perse", "picka", "pierdol*", "pillu*", "pimmel", "piss*", "pizda", "poontsee", "poop", "porn", "p0rn", "pr0n", "preteen", "pula", "pule", "puta", "puto", "qahbeh", "queef*", "rautenberg", "schaffer", "scheiss*", "schlampe", "schmuck", "screw", "sh!t*", "sharmuta", "sharmute", "shipal", "shiz", "skribz", "skurwysyn", "sphencter", "spic", "spierdalaj", "splooge", "suka", "b00b*", "testicle*", "twat", "vittu", "wank*", "wetback*", "wichser", "wop*", "yed", "zabourah", "pidhi", "pirdhu", "pidh", "pidhuc", "kari", "kar", "karuc", "piqk", "piqken", "muti", "mut", "pshurq", "shurr", "karlesh", "qirje", "gay", "bitch", "snitch", "qifsha", "gojore", "hanksh mutin", "mutin", "qifsha nanen", "qifsha motren", "qifsha familjen", "ta qifsha", "qifsha", "hanksh karin", "qifsha ropt", "seks", "sex", "porn", "xnxx", "x n x x", "x nxx", "pornhub", "kaka", "seksi", "qihemi", "qihem", "qifsha", "xvideos", ];
@@ -396,7 +396,7 @@ function createText(user, message, time, username1, key) {
     let attribute6 = document.createAttribute("oncontextmenu");
     let attribute7 = document.createAttribute("data-key");
     let info = `${user} \n\n ${time}`;
-    attribute1.value = `message-id-${darkIndex}`;
+    attribute1.value = `message-id-${key}`;
     attribute2.value = "message-class animate__bounceIn";
     attribute3.value = `${user} \n\n ${time}`;
     attribute4.value = `You are mentioned in this message!\n\n ${info}`;
@@ -434,6 +434,10 @@ function createText(user, message, time, username1, key) {
             }
         } else {}
     }
+    $("#ulist").animate({
+        scrollTop: $("#ulist").prop("scrollHeight")
+    }, 0);
+    document.getElementById('ulist').scrollTop =  document.getElementById('ulist').scrollHeight;
 }
 
 function checkRoom() {
@@ -525,6 +529,7 @@ function sendMessage() {
                 username: username,
             });
             document.getElementById("message").value = "";
+            playAudio("sendMessage");
             disableChat();
             setTimeout(() => {
                 enableChat();
@@ -534,6 +539,15 @@ function sendMessage() {
         return false;
     }
 }
+
+firebase.database().ref("rooms/" + room + "/chats").on("child_removed", function (snapshot) {
+    document.getElementById(`message-id-${snapshot.key}`).innerText = "This message has been deleted!";
+    setTimeout(() => {
+        document.getElementById(`message-id-${snapshot.key}`).classList.add("hidden");
+    }, 5000);
+});
+
+
 
 let browserName;
 fnBrowserDetect();
@@ -571,11 +585,6 @@ query.on("child_added", function (snapshot) {
     let times = snapshot.val().time;
     let postKey = snapshot.key;
     createText(owner, message, times, username, postKey);
-    $("#ulist").animate({
-        scrollTop: $("#ulist").prop("scrollHeight")
-    }, 0);
-    document.getElementById('ulist').scrollTop =  document.getElementById('ulist').scrollHeight
-
 });
 
 rooms.on("child_added", function (snapshot) {
@@ -971,7 +980,7 @@ function deleteChat() {
                             showConfirmButton: false,
                             timer: 1500,
                         });
-                        location.reload();
+                        
                     } else {
                         Swal.fire({
                             position: "top-end",
@@ -1043,7 +1052,7 @@ recognition.addEventListener("result", (e) => {
     speech = false;
     document.getElementById("speechToText").style.color = "black";
     document.getElementById("message").value = transcript;
-    console.log(transcript);
+    //console.log(transcript);
 });
 
 if (speech == true) {
@@ -1151,11 +1160,11 @@ function checkResults(id) {
         let clean = response.data.attributes.last_analysis_stats.harmless;
         if (malware >= 3 || suspicious >= 10) {
             console.log("This might be a malware!");
-        } else if (malware >= 7) {
+        } else if (malware >= 5) {
             console.log("This is a malware!");
             closeUnsafeLink();
         } else {
-            console.log("This is a safe link! " + clean + " antiviruses said this link is safe!");
+            //console.log("This is a safe link! " + clean + " antiviruses said this link is safe!");
         }
 
     }).catch((err) => console.error(err));
