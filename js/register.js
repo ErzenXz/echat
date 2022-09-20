4cde4511679a7eb59c73c660cd77c454bbd9432d
ErzenXs <njnana2017@gmail.com>
2022-09-20 19:47:50 +0200

    Login fixes

--- js/register.js
+++ js/register.js
@@ -287,7 +287,7 @@ firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    //location.replace("./");
+    location.replace("./");
     // ...
   } else {
     // User is signed out
